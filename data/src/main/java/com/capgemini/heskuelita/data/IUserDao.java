f329cac6dce784f8685bc0181f1c2f71a821a596
Jessica Abril <40779082+jeabril@users.noreply.github.com>
2019-03-17 18:35:48 +0200

    Update IUserDao.java

--- data/src/main/java/com/capgemini/heskuelita/data/IUserDao.java
+++ data/src/main/java/com/capgemini/heskuelita/data/IUserDao.java
@@ -1,9 +1,15 @@
 package com.capgemini.heskuelita.data;
+import com.capgemini.heskuelita.core.Beans.StudentAnnotation;
 
-import com.capgemini.heskuelita.core.Beans.User;
+import java.util.List;
 
-public class IUserDao {
+public interface IStudentDao {
 
-    User login (String userName, String password);
+
+    void create (String first_name, String last_name, String type_id, String number_id, String gender, String phone_number);
+    List<StudentAnnotation> findAll();
+    UserAnnotation findById (int id);
+    void update (StudentAnnotation user);
+    boolean remove (StudentAnnotation us);
 
 }
